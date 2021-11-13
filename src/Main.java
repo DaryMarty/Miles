009edb1b19b8e6bf2570bf748740c16ef3bc66bf
Darya Martemyanova <mdy.89@mail.ru>
2021-11-13 20:01:22 +0300

    Initial commit

---
+++ src/Main.java
@@ -0,0 +1,8 @@
+public class Main {
+    public static void main(String[] args) {
+        int ticketPrice = 5000;
+        int bonusMileCost = 20;
+        int numberOfBonusMiles = ticketPrice / bonusMileCost;
+        System.out.println(numberOfBonusMiles);
+    }
+}
